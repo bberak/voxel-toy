390f3649b5a268e6cfa2631dcf49bc27a7fc12c7
bberak <bberak@gmail.com>
2019-06-23 23:15:35 +1000

    Saving work

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
+const args = require("args");
 
-const main = () => {
-	console.log("Here :)")
+const question = str => {
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+
+	return new Promise(resolve => {
+		rl.question(str, resolve);
+	}).then(answer => {
+		rl.close();
+		return answer;
+	});
+};
+
+async function main() {
+	args.option("input", "The path to the input .vox file");
+
+	const flags = args.parse(process.argv);
+
+	console.log(flags);
 }
 
-main();
+main().catch(console.error);
