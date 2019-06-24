59fe4b1fd9109250d607bd1ab6e7b355e79b9490
bberak <bberak@gmail.com>
2019-06-24 11:27:45 +1000

    Worked on reading args and voxels

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
 const args = require("args");
-
-const question = str => {
-	const rl = readline.createInterface({
-		input: process.stdin,
-		output: process.stdout
-	});
-
-	return new Promise(resolve => {
-		rl.question(str, resolve);
-	}).then(answer => {
-		rl.close();
-		return answer;
-	});
-};
+const { question, abolsutePath, changeExtension, readBuffer } = require("./utils");
+const assert = require("assert");
+const parseVoxels = require("vox-reader");
 
 async function main() {
-	args.option("input", "The path to the input .vox file");
+	args
+		.option("input", "The path to the input .vox file")
+		.option("scale", "The scale of each individual voxel when converted to a UE4 box geometry (default 100)")
 
 	const flags = args.parse(process.argv);
 
-	console.log(flags);
+	const input = flags.input || (await question("Where is the .vox file?"));
+	const scale = flags.scale || (await question("What is the scale of each individual voxel in UE4 units?")) || 100;
+
+	assert(input, "input must be provided");
+
+	const voxFile = abolsutePath(input);
+	const t3dFile = changeExtension(voxFile, ".t3d");
+
+	console.log({
+		voxFile,
+		t3dFile,
+		scale
+	})
+
+	const voxBuffer = readBuffer(voxFile);
+	const voxels = parseVoxels(voxBuffer);
 }
 
 main().catch(console.error);
---
+++ src/utils.js
@@ -0,0 +1,42 @@
+const readline = require("readline");
+const path = require("path");
+const fs = require("fs");
+
+const question = str => {
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+
+	return new Promise(resolve => {
+		rl.question(str.trim() + " ", resolve);
+	}).then(answer => {
+		rl.close();
+		return answer;
+	});
+};
+
+const abolsutePath = file => {
+	return path.isAbsolute(file) ? file : path.join(process.cwd(), file);
+};
+
+const changeExtension = (file, newExt) => {
+	const originalExt = path.extname(file);
+
+	return originalExt ? file.replace(originalExt, newExt) : file + newExt;
+};
+
+const readBuffer = file => {
+	return new Promise((resolve, reject) => {
+		fs.readFile(file, (error, buffer) => {
+			if (error) reject(error);
+			else resolve(buffer);
+		});
+	});
+};
+
+module.exports = {
+	question,
+	abolsutePath,
+	changeExtension
+};
